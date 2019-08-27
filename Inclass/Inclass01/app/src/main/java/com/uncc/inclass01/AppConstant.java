c80967c476538d142e8227d3f6d8b2b2bd551d7e
Khoi Bui <kbui8@uncc.edu>
2019-08-27 19:32:19 -0400

    Update dependencies

--- Inclass/Inclass01/app/src/main/java/com/uncc/inclass01/AppConstant.java
+++ Inclass/Inclass01/app/src/main/java/com/uncc/inclass01/AppConstant.java
@@ -1,7 +1,5 @@
 package com.uncc.inclass01;
 
-import android.content.Intent;
-
 public class AppConstant {
     public static final int LOGIN_CODE = 100;
     public static final int SIGNUP_CODE = 200;
@@ -11,7 +9,7 @@ public class AppConstant {
     public static final String CHAT = "CHAT";
     public static final String CHAT_USERS = "CHAT_USERS";
     public static final String CHATROOM_DB_KEY = "chatrooms";
-    public static final String USER_DB_KEY = "users";
+    public static final String USER_DB_KEY = "userProfiles";
     public static final String CHATROOM_ID = "id";
     public static final String CHATROOM_NAME = "name";
 }
