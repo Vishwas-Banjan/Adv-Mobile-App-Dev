0282c3c9343e42bf6371be970000ebe306bd5120
Khoi Bui <kbui8@uncc.edu>
2019-10-15 21:15:56 -0400

    Filter by beacons

---
+++ Inclass/Inclass05/android/app/src/main/java/com/mobility/inclass04/Utils/Filter.java
@@ -0,0 +1,28 @@
+package com.mobility.inclass04.Utils;
+
+public class Filter {
+
+    private String key;
+    private String value;
+
+    public Filter(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
---
+++ Inclass/Inclass05/android/app/src/main/java/com/mobility/inclass04/Utils/LimitedSizeQueue.java
@@ -0,0 +1,20 @@
+package com.mobility.inclass04.Utils;
+
+import java.util.ArrayList;
+
+public class LimitedSizeQueue<K> extends ArrayList<K> {
+
+    private int maxSize;
+
+    public LimitedSizeQueue(int size){
+        this.maxSize = size;
+    }
+
+    public boolean add(K k){
+        boolean r = super.add(k);
+        if (size() > maxSize){
+            removeRange(0, size() - maxSize);
+        }
+        return r;
+    }
+}
